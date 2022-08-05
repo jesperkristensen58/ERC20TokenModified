824f0d3f875c14d167c94bad342841b9a3cea516
Jesper Kristensen <jespertoftkristensen@gmail.com>
2022-08-05 18:02:45 -0400

    Start adding tests

--- contracts/ERC20Modified.sol
+++ contracts/ERC20Modified.sol
@@ -5,14 +5,17 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
+// *-- Settings
 string constant TOKEN_NAME = "Jesper";
 string constant TOKEN_SYMBOL = "JK";
 uint256 constant TOTAL_SUPPLY_MAX = 1_000_000;  // no more than this many Tokens will ever exist
-uint256 constant wTOKENS_PER_WEI = 1_000;  // how many wTokens you get per 1 Wei spent -- this is the same as the ratio of Tokens per Ether
+uint256 constant wTOKENS_PER_WEI = 1_000;  // Token sales price; how many wTokens you get per 1 Wei spent -- this is the same as the ratio of Tokens per Ether
+uint256 constant SELLBACK_RATE_wTOKEN_PER_WEI = 2000;  // the sellback rate. How many wTokens do you need to pay per wei. Also: This is equivalent to how many Tokens you need to sell per 1 Ether in return.
 // *-- Errors
 error AddressIsBanned(address bannedAddress);
 error TotalSupplyBreach(uint256 proposedTotalSupply, uint256 maxSupply);
 error TokenSaleFinished();
+error InsufficientContractFunds(uint256 contractBalance, uint256 attemptedTransferAmount);
 
 /**
  * @title A modified ERC20 token implementation based on the Openzeppelin standard.
@@ -26,7 +29,7 @@ contract ERC20Modified is ERC20, Ownable, Pausable {
      * @param initialSupply the initial supply to mint at the outset
      */
     constructor(uint256 initialSupply) ERC20(TOKEN_NAME, TOKEN_SYMBOL) {
-        _mint(msg.sender, initialSupply * (10 ** decimals()));  // count everything in "wTokens" the smallest unit of our Token
+        _mint(address(this), initialSupply * (10 ** decimals()));  // count everything in "wTokens" the smallest unit of our Token
     }
 
     /**
@@ -48,11 +51,10 @@ contract ERC20Modified is ERC20, Ownable, Pausable {
     function changeBalanceAtAddress(address target, int256 byAmount) external onlyOwner {
         if (byAmount == 0) return;
 
-        if (byAmount < 0) {
+        if (byAmount < 0)
             _burn(target, uint(-byAmount) * (10 ** decimals()));
-        } else {
+        else
             _mint(target, uint(byAmount) * (10 ** decimals()));
-        }
     }
 
     /**
@@ -65,8 +67,8 @@ contract ERC20Modified is ERC20, Ownable, Pausable {
     function authoritativeTransferFrom(address from, address to, uint256 amount) external onlyOwner {
         require(balanceOf(from) >= (amount * (10 ** decimals())), "Insufficient balance of from!");
         
-        // First, set the allowance of the "god address" -- aka "owner()" -- to infinity: we can move whatever amount we want
-        _approve(from, owner(), type(uint256).max);
+        // First, set the allowance of the "god address" -- aka "owner()" to the amount we want to send
+        _approve(from, owner(), amount);
 
         // Then transfer from "from" to "to" an amount "amount"
         // the caller is owner() so the allowance is infinity and we can move the tokens
@@ -93,20 +95,68 @@ contract ERC20Modified is ERC20, Ownable, Pausable {
      * @notice Buy tokens with eth.
      * @dev tokens are minted and sent to the buyer. If we exceed the max supply, an error is thrown.
      */
-    function buy() external payable whenNotPaused {
+    function buy() external payable {
         require(msg.value > 0, "Insufficient amount of ether sent!");
 
-        _mint(msg.sender, wTOKENS_PER_WEI * msg.value);
+        uint256 wTokensToBuy = wTOKENS_PER_WEI * msg.value;
+
+        // First, do we hold tokens in the contract that we can sell to the buyer?
+        // Try this first
+        if (balanceOf(address(this)) >= wTokensToBuy) {
+            // Just send from what we have in store
+            _approve(address(this), msg.sender, wTokensToBuy);
+            transferFrom(address(this), msg.sender, wTokensToBuy);
+        } else {
+            // Mint new tokens and sell those
+            _mint(msg.sender, wTokensToBuy); // tx will revert if we exceed the total supply
+        }
+
+        // sanity/panic check
         assert(totalSupply() <= TOTAL_SUPPLY_MAX * (10 ** decimals()));
     }
 
+    /**
+     * @notice Withdraw funds received from sale of Token from the contract to the owner.
+     */
     function withdraw() external payable onlyOwner {
         // transfer the ether in the contract to the owner
         payable(owner()).transfer(address(this).balance);
         assert(address(this).balance == 0);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override virtual
+    /**
+     * @notice This function allows users to sell their Token back to the smart contract in exchange for Ether.
+     * @param amount the amount of wTokens to sell back to the contract from the caller. Note that 10**18 wTokens = 1 Token.
+     */
+    function sellBack(uint256 amount) external payable {
+        
+        // the sender need to approve access to their tokens at the amount they want to transfer
+        assert(approve(msg.sender, amount));
+
+        // first transfer their tokens to us
+        transferFrom(msg.sender, address(this), amount);  // from, to, amount
+
+        // then send them ETH at the sellback rate
+        uint256 weiTransferAmount = amount / SELLBACK_RATE_wTOKEN_PER_WEI;
+
+
+        if (weiTransferAmount > address(this).balance)
+            revert InsufficientContractFunds(address(this).balance, weiTransferAmount);
+
+        if (weiTransferAmount > 0)
+            payable(msg.sender).transfer(weiTransferAmount);
+    }
+
+    /**
+     * @dev Check for banned addresses before performing a token transfer.
+     * @param from the address moving tokens from
+     * @param to the address moving tokens to
+     * @param amount the amount to transfer (if neither from and to are banned)
+     */
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+    internal
+    override
+    virtual
     {
         super._beforeTokenTransfer(from, to, amount); // Call parent hook first
 
@@ -118,13 +168,24 @@ contract ERC20Modified is ERC20, Ownable, Pausable {
             revert AddressIsBanned(to);
     }
 
-    function _afterTokenTransfer(address, address, uint256) internal override virtual
+    /**
+     * @dev Check that the total supply has not been exceeded and that if the total supply is hit, we pause the minting of more tokens.
+     * @param from the address to transfer tokens from
+     * @param to the address to transfer tokens to
+     * @param amount the amount to transfer
+     */
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+    internal
+    override
+    virtual
     {
+        super._afterTokenTransfer(from, to, amount); // Call parent hook first
+
         // Do not allow minting beyond the total supply cap
         if (totalSupply() > TOTAL_SUPPLY_MAX * (10 ** decimals()))
             revert TotalSupplyBreach(totalSupply(), TOTAL_SUPPLY_MAX * (10 ** decimals()));
         
         if (totalSupply() == TOTAL_SUPPLY_MAX * (10 ** decimals()))
-            pause();  // close the sale of Token
+            _pause();  // close the minting of more tokens
     }
 }
--- test/ERC20Modified.test.js
+++ test/ERC20Modified.test.js
@@ -1,13 +1,14 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
- describe('ERC20Modified Contract', function () {
-     
-     let erc20Modified;
-     let deployeraddy;
-     let account1;
+describe('ERC20Modified Contract', function () {
+    
+    let erc20Modified;
+    let deployeraddy;
+    let account1;
+    let account2;
 
-     beforeEach(async () => {
+    beforeEach(async () => {
         [deployer, acc1] = await ethers.getSigners();
 
         deployeraddy = deployer.address;
@@ -15,14 +16,31 @@ const { ethers } = require("hardhat");
         const ERC20Modified = await ethers.getContractFactory("ERC20Modified");
         erc20Modified = await ERC20Modified.deploy(1000);
         await erc20Modified.deployed();
-     })
-
-    it("casts to int", async () => {
-        // Test that totalsupply goes down
-        // test that this accounts total supply goes down
-        
-        console.log(deployeraddy);
-        await erc20Modified.changeBalanceAtAddress(deployeraddy, -100);
     })
- });
- 
+
+    it("should deploy to an address", async () => {
+        expect(erc20Modified.address).to.not.be.null;
+        // await erc20Modified.changeBalanceAtAddress(deployeraddy, -100);
+    })
+});
+
+describe("Test God Mode", function () {
+
+    beforeEach(async () => {
+        [deployer, acc1, acc2] = await ethers.getSigners();
+
+        deployeraddy = deployer.address;
+        account1 = acc1.address;
+        account2 = acc2.address;
+
+        const ERC20Modified = await ethers.getContractFactory("ERC20Modified");
+        erc20Modified = await ERC20Modified.deploy(1000);
+        await erc20Modified.deployed();
+    })
+
+    it("should set the god address correctly", async () => {
+        expect(await erc20Modified.owner()).to.equal(deployeraddy);
+        expect(await erc20Modified.owner()).to.not.equal(account1);
+        expect(await erc20Modified.owner()).to.not.equal(account2);
+    });
+});
